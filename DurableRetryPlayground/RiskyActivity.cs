4df93744aeb32ed0088e3b4dfaa2a8c88fa0fe5c
savbace <7579898+savbace@users.noreply.github.com>
2023-06-13 18:27:08 +0300

    RiskyActivitySync

--- DurableRetryPlayground/RiskyActivity.cs
+++ DurableRetryPlayground/RiskyActivity.cs
@@ -1,22 +1,29 @@
 ﻿using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 
-namespace DurableRetryPlayground
+namespace DurableRetryPlayground;
+
+public class RiskyActivity
 {
-    public class RiskyActivity
+    private readonly ILogger<RiskyActivity> logger;
+
+    public RiskyActivity(ILogger<RiskyActivity> logger)
     {
-        private readonly ILogger<RiskyActivity> logger;
+        this.logger = logger;
+    }
 
-        public RiskyActivity(ILogger<RiskyActivity> logger)
-        {
-            this.logger = logger;
-        }
+    [Function(nameof(RiskyActivity))]
+    public async Task Run([ActivityTrigger] BackgroundOptions options)
+    {
+        this.logger.LogInformation("Running with priority {priority}.", options.Priority);
+        await Task.Delay(3000);
+        throw new ApplicationException("Failed to run!");
+    }
 
-        [Function(nameof(RiskyActivity))]
-        public void Run([ActivityTrigger] BackgroundOptions options)
-        {
-            this.logger.LogInformation("Running with priority {priority}.", options.Priority);
-            throw new ApplicationException("Failed to run!");
-        }
+    [Function("RiskyActivitySync")]
+    public void RunSync([ActivityTrigger] BackgroundOptions options)
+    {
+        this.logger.LogInformation("Sync: Running with priority {priority}.", options.Priority);
+        throw new ApplicationException("Sync: Failed to run!");
     }
 }
